ececf03f20b7b399c01e3576c15ffe753bfa2d84
Vijay Nadella <30449746+vijaykirann@users.noreply.github.com>
2023-07-06 15:06:18 +0530

    Add files via upload

--- plexbuilder.py
+++ plexbuilder.py
@@ -9,12 +9,17 @@ docker_repository = 'vijaykirann/plex-armv7'
 dockerfile = 'Dockerfile.armv7'
 docker_tag = 'latest'
 
+# clones the repository form GIT
 subprocess.run(['git', 'clone', repository_url, destination_folder])
 os.chdir(destination_folder)
+
+# runs the docker build and creates the image
 subprocess.run(['docker', 'build', '-t', docker_repository + ':' + docker_tag, '-f', dockerfile, '.'])
 
+#login to docker hub
 docker_login_cmd = 'docker login -u ' + docker_username + ' -p ' + docker_password
 subprocess.run(docker_login_cmd, shell=True)
 
+#Pushes the image to dockerhub and logs out
 subprocess.run(['docker', 'push', docker_repository + ':' + docker_tag])
 subprocess.run(['docker', 'logout'])
